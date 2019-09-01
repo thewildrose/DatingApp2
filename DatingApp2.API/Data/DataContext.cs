a2acdfb3b27e8a8b17da2406101dc5c249a0bbca
unknown <mmilica472@gmail.com>
2019-09-01 11:01:50 +0200

    Initial Commit

---
+++ DatingApp2.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp2.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp2.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
