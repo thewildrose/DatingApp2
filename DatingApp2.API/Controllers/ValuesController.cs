0ec2edd162641a5cd350c8bbd84ff10e20cbbaf0
unknown <mmilica472@gmail.com>
2019-09-04 01:01:19 +0200

    End of Section 3

--- DatingApp2.API/Controllers/ValuesController.cs
+++ DatingApp2.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp2.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp2.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -28,6 +30,7 @@ namespace DatingApp2.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
--- DatingApp2.API/Data/DataContext.cs
+++ DatingApp2.API/Data/DataContext.cs
@@ -8,6 +8,7 @@ namespace DatingApp2.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
 
     }
 }
